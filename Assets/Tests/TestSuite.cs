026b0d5d534b04211a9cdfab36613cb44a104eda
Marcus Mouyat <marcus@mouyat.com>
2019-08-07 16:12:15 +1000

    Accomplished more stuff I guess

--- Assets/Tests/TestSuite.cs
+++ Assets/Tests/TestSuite.cs
@@ -11,25 +11,88 @@ public class TestSuite
     private Player player;
 
     [SetUp]
-    void Setup()
+    public void Setup()
     {
         GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
         game = Object.Instantiate(prefab);
 
         gameManager = GameManager.Instance;
-        player = Object.FindObjectOfType<Player>();
+        //player = Object.FindObjectOfType<Player>();
         player = game.GetComponentInChildren<Player>();
     }
 
     [UnityTest]
-    IEnumerator GamePrefabLoaded()
+    public IEnumerator GamePrefabLoaded()
     {
         yield return new WaitForEndOfFrame();
-        Assert.NotNull(game, "Oi cunt, the player gameObject's gone walkabout! Strewth, mate!");
+        Assert.NotNull(game, "Oi cunt, the game prefab's gone walkabout! Strewth, mate!");
+    }
+
+    public IEnumerator PlayerExists()
+    {
+        yield return new WaitForEndOfFrame();
+        Assert.NotNull(player, "The player doesn't exist! Fix this, maggot!");
+    }
+
+    /*
+    [UnityTest]
+    IEnumerator ItemCollidesWithPlayer()
+    {
+        yield return new WaitForEndOfFrame();
+        GameObject itemPrefab = Resources.Load<GameObject>("Prefabs/Entities/Item");
+    }
+    */
+
+    [UnityTest]
+    public IEnumerator ItemCollidesWithPlayer()
+    {
+        //Item item = gameManager.itemManager.GetItem(0);
+        //item.transform.position = player.transform.position;
+
+        GameObject itemPrefab = Resources.Load<GameObject>("Prefabs/Entities/Item");
+
+        Vector3 playerPosition = player.transform.position;
+        GameObject item = Object.Instantiate(itemPrefab, playerPosition, Quaternion.identity);
+
+
+
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForEndOfFrame();
+
+
+
+        Assert.IsTrue(item == null);
+    }
+
+    [UnityTest]
+    public IEnumerator ItemCollectedAndScoreAdded()
+    {
+        // Spawn an item (same as above)
+        // Record old score in an int
+
+        // WaitForFixedUpdate
+        // WaitForEndOfFrame
+
+        // Assert IsTrue old score != new score
+
+
+        GameObject itemPrefab = Resources.Load<GameObject>("Prefabs/Entities/Item");
+
+        Vector3 playerPosition = player.transform.position;
+        GameObject item = Object.Instantiate(itemPrefab, playerPosition, Quaternion.identity);
+
+        int oldScore = gameManager.score;
+
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForEndOfFrame();
+
+        int newScore = gameManager.score;
+
+        Assert.IsTrue(newScore > oldScore);
     }
 
     [TearDown]
-    void Teardown()
+    public void Teardown()
     {
         Object.Destroy(gameManager);
     }
