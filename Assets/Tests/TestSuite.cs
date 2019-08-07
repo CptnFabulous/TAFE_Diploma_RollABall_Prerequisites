40fac3761bc5e842a0575e421e292b84e2ec87d5
Marcus Mouyat <marcus@mouyat.com>
2019-08-07 13:06:49 +1000

    Added files to repository

---
+++ Assets/Tests/TestSuite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TestSuite
+{
+    private GameObject game;
+    private GameManager gameManager;
+    private Player player;
+
+    [SetUp]
+    void Setup()
+    {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
+        game = Object.Instantiate(prefab);
+
+        gameManager = GameManager.Instance;
+        player = Object.FindObjectOfType<Player>();
+        player = game.GetComponentInChildren<Player>();
+    }
+
+    [UnityTest]
+    IEnumerator GamePrefabLoaded()
+    {
+        yield return new WaitForEndOfFrame();
+        Assert.NotNull(game, "Oi cunt, the player gameObject's gone walkabout! Strewth, mate!");
+    }
+
+    [TearDown]
+    void Teardown()
+    {
+        Object.Destroy(gameManager);
+    }
+}
